196ea5a23014192ce7be73667a135ec1b9a5646d
Joseph Kniskern <kniskernjoseph@yahoo.com>
2022-09-20 11:11:10 -0400

    initial css complete

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function App() {
-  const [theme, setTheme] = useState("red");
-  return null;
-}
-
-export default App;
---
+++ src/components/Recipe.js
@@ -0,0 +1,35 @@
+import React from "react";
+import IngredientList from "./IngredientList";
+
+export default function Recipe(props) {
+  const { name, cookTime, servings, instuctions, ingredients } = props;
+  return (
+    <div className="recipe">
+      <div className="recipe__header">
+        <h3 className="recipe__title">{name}</h3>
+        <div>
+          <button className="btn btn--primary mr-1">Edit</button>
+          <button className="btn btn-dander mr-1">Delete</button>
+        </div>
+        <div>
+          <span>Cook Time:</span>
+          <span>{cookTime}</span>
+        </div>
+        <div>
+          <span>Serving:</span>
+          <span>{servings}</span>
+        </div>
+        <div>
+          <span>Instructions:</span>
+          <div>{instuctions}</div>
+        </div>
+        <div>
+          <span>Ingredients:</span>
+          <div>
+            <IngredientList ingredients={ingredients} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
