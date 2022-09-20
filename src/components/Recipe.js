17554e2c8f69192d3286eb6efcf1f34948c642f5
Joseph Kniskern <kniskernjoseph@yahoo.com>
2022-09-20 11:46:39 -0400

    recipe-list css complete

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -5,23 +5,27 @@ export default function Recipe(props) {
   const { name, cookTime, servings, instuctions, ingredients } = props;
   return (
     <div className="recipe">
-      <div className="recipe__header">
-        <h3 className="recipe__title">{name}</h3>
+      <div>
+        <div className="recipe__header">
+          <h3 className="recipe__title">{name}</h3>
+        </div>
         <div>
           <button className="btn btn--primary mr-1">Edit</button>
-          <button className="btn btn-dander mr-1">Delete</button>
+          <button className="btn btn--danger mr-1">Delete</button>
         </div>
-        <div>
-          <span>Cook Time:</span>
-          <span>{cookTime}</span>
+        <div className="recipe__row">
+          <span className="recipe__label">Cook Time:</span>
+          <span className="recipe__value">{cookTime}</span>
         </div>
-        <div>
-          <span>Serving:</span>
-          <span>{servings}</span>
+        <div className="recipe__row">
+          <span className="recipe__label">Serving:</span>
+          <span className="recipe__value">{servings}</span>
         </div>
-        <div>
-          <span>Instructions:</span>
-          <div>{instuctions}</div>
+        <div className="recipe__row">
+          <span className="recipe__label">Instructions:</span>
+          <div className="recipe__value recipe__instructions recipe__value--indented">
+            {instuctions}
+          </div>
         </div>
         <div>
           <span>Ingredients:</span>
