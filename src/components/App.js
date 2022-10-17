ba9eab2667d48e9616199827e11b4313fa98f2bd
Joseph Kniskern <kniskernjoseph@yahoo.com>
2022-10-17 11:54:06 -0400

    add and delete button functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import RecipeList from "./RecipeList";
-import "../css/app.css"
+import "../css/app.css";
 
 function App() {
-  return <RecipeList recipes={sampleRecipes} />;
+  const [recipes, setRecipes] = useState(sampleRecipes);
+
+  function handleRecipeAdd() {
+    const newRecipe = {
+      id: Date.now,
+      name: "New",
+      servings: 1,
+      cookTime: "1:00",
+      instructions: "Instructions",
+      ingredients: [{ id: Date.now, name: "Name", amount: "1 Tbs" }],
+    };
+
+    setRecipes([...recipes, newRecipe]);
+  }
+
+  function handleRecipeDelete(id) {
+    setRecipes(recipes.filter((recipe) => recipe.id !== id));
+  }
+
+  return (
+    <RecipeList
+      recipes={recipes}
+      handleRecipeAdd={handleRecipeAdd}
+      handleRecipeDelete={handleRecipeDelete}
+    />
+  );
 }
 
 const sampleRecipes = [
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -2,7 +2,15 @@ import React from "react";
 import IngredientList from "./IngredientList";
 
 export default function Recipe(props) {
-  const { name, cookTime, servings, instuctions, ingredients } = props;
+  const {
+    id,
+    name,
+    cookTime,
+    servings,
+    instuctions,
+    ingredients,
+    handleRecipeDelete,
+  } = props;
   return (
     <div className="recipe">
       <div>
@@ -11,7 +19,12 @@ export default function Recipe(props) {
         </div>
         <div>
           <button className="btn btn--primary mr-1">Edit</button>
-          <button className="btn btn--danger mr-1">Delete</button>
+          <button
+            className="btn btn--danger mr-1"
+            onClick={() => handleRecipeDelete(id)}
+          >
+            Delete
+          </button>
         </div>
         <div className="recipe__row">
           <span className="recipe__label">Cook Time:</span>
